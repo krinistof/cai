4a0d2d1af4cf9d9bfb9fb966d2d0cc27e8b5bf13
krinistof <kristof@mntr.dev>
2023-11-26 04:38:13 +0100

    add `-e` flag

--- src/common.rs
+++ src/common.rs
@@ -9,6 +9,92 @@ use std::{
     io::{Read, Write},
     path::PathBuf,
 };
+use clap::Parser;
+use lazy_static::lazy_static;
+
+#[derive(Parser)]
+#[command(about)]
+#[group(id = "command", args(vec![ "input_path", "example" ]))]
+pub struct Args {
+    #[arg(short = 'x', long)]
+    pub execute: bool,
+    #[arg(short = 'e', long, group = "input")]
+    pub example: bool,
+    #[arg(require_equals = true, group = "input")]
+    pub input_path: Option<PathBuf>,
+}
+
+impl Args {
+}
+
+pub trait ClientExt {
+    fn process_request(&self, req: &mut ChatCompletionRequest, args: &Args) -> Result<()>;
+    fn eval(self, args: Args) -> Result<()>;
+}
+
+impl ClientExt for Client {
+    fn eval(self, args: Args) -> Result<()> {
+        let Args {
+            ref input_path,
+            example,
+            ..
+        } = args;
+        
+        if let Some(ref input_path) = input_path {
+            let contents = read_file(input_path)?;
+            let mut req = toml::from_str(&contents)?;
+
+            self.process_request(&mut req, &args)?;
+
+            write_to_file(input_path, &toml::to_string(&req)?)
+        } else {
+            if example{
+                lazy_static! {
+                    static ref FILE_PATH: PathBuf = PathBuf::from("./new.toml");
+                };
+                const EXAMPLE_CONTENT: &str = include_str!("../examples/life.toml");
+
+                dbg!(&FILE_PATH.as_path());
+                write_to_file(&FILE_PATH, EXAMPLE_CONTENT)
+            } else { Ok(()) }
+
+        }
+    }
+    fn process_request(&self, req: &mut ChatCompletionRequest, args: &Args) -> Result<()> {
+        let Args {
+            execute,
+            example,
+            ..
+        } = args;
+
+        // TODO codesmell: match refactor needed
+        if *example {
+            lazy_static! {
+                static ref FILE_PATH: PathBuf= PathBuf::from("new.toml");
+            };
+            const EXAMPLE_CONTENT: &str = include_str!("../examples/life.toml");
+
+            return write_to_file(&FILE_PATH, EXAMPLE_CONTENT);
+        }
+
+        if *execute {
+            if let Some(command) = get_command(req)? {
+                let output = run(&command)?;
+                req.messages.push(ChatCompletionMessage {
+                    role: MessageRole::user,
+                    content: format!("output of command:\n```\n{output}\n```\n"),
+                    name: None,
+                    function_call: None,
+                });
+            }
+        } else {
+            // TODO move continue_chat to ClientExt trait from dependency
+            self.continue_chat(req)?;
+        }
+
+        Ok(())
+    }
+}
 
 pub fn read_file(input_path: &PathBuf) -> Result<String> {
     let mut file = File::open(input_path)?;
@@ -19,6 +105,7 @@ pub fn read_file(input_path: &PathBuf) -> Result<String> {
 
 pub fn write_to_file(input_path: &PathBuf, contents: &str) -> Result<()> {
     let mut file = File::options()
+        .create(true)
         .write(true)
         .truncate(true)
         .open(input_path)?;
@@ -26,23 +113,3 @@ pub fn write_to_file(input_path: &PathBuf, contents: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn process_request(
-    client: &Client,
-    req: &mut ChatCompletionRequest,
-    execute: bool,
-) -> Result<()> {
-    if execute {
-        if let Some(command) = get_command(req)? {
-            let output = run(&command)?;
-            req.messages.push(ChatCompletionMessage {
-                role: MessageRole::user,
-                content: format!("output of command: {output}"),
-                name: None,
-                function_call: None,
-            });
-        }
-    } else {
-        client.continue_chat(req)?;
-    }
-    Ok(())
-}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-use crate::common::{process_request, read_file, write_to_file};
+use common::{Args, ClientExt};
 use anyhow::{Context, Result};
-use clap::Parser;
 use dotenv::dotenv;
 use openai_api_rs::v1::api::Client;
-use std::{env, path::PathBuf};
+use clap::Parser;
+use std::env;
 
 mod common;
 mod execute;
-
-#[derive(Parser)]
-#[command(about)]
-struct Args {
-    #[arg(short = 'x', long)]
-    execute: bool,
-    #[arg(require_equals = true)]
-    input_path: PathBuf,
-}
 
 fn main() -> Result<()> {
     dotenv()?;
@@ -24,17 +15,5 @@ fn main() -> Result<()> {
         env::var("OPENAI_API_KEY").with_context(|| "No OPENAI_API_KEY env var given!")?,
     );
 
-    let Args {
-        input_path,
-        execute,
-    } = Args::parse();
-
-    let contents = read_file(&input_path)?;
-    let mut req = toml::from_str(&contents)?;
-
-    process_request(&client, &mut req, execute)?;
-
-    write_to_file(&input_path, &toml::to_string(&req)?)?;
-
-    Ok(())
+    client.eval(Args::parse())
 }
