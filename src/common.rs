218597b88a97484e05df0cdd016cf5b0e0f1a224
krinistof <kristof@mntr.dev>
2023-11-26 05:14:08 +0100

    refactor(common.rs): match instead of ifs

--- src/common.rs
+++ src/common.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use lazy_static::lazy_static;
 use openai_api_rs::v1::{
     api::Client,
-    chat_completion::{ChatCompletionMessage, ChatCompletionRequest, MessageRole},
+    chat_completion::{ChatCompletionMessage, MessageRole},
 };
 use std::{
     fs::File,
@@ -27,70 +27,50 @@ pub struct Args {
 impl Args {}
 
 pub trait ClientExt {
-    fn process_request(&self, req: &mut ChatCompletionRequest, args: &Args) -> Result<()>;
     fn eval(self, args: Args) -> Result<()>;
 }
 
 impl ClientExt for Client {
     fn eval(self, args: Args) -> Result<()> {
-        let Args {
-            ref input_path,
-            example,
-            ..
-        } = args;
-
-        if let Some(ref input_path) = input_path {
-            let contents = read_file(input_path)?;
-            let mut req = toml::from_str(&contents)?;
-
-            self.process_request(&mut req, &args)?;
-
-            write_to_file(input_path, &toml::to_string(&req)?)
-        } else {
-            if example {
+        match args {
+            Args { example, .. } if example => {
                 lazy_static! {
-                    static ref FILE_PATH: PathBuf = PathBuf::from("./new.toml");
+                    static ref FILE_PATH: PathBuf = PathBuf::from("new.toml");
                 };
+                let path = FILE_PATH.display();
+                println!("Example loaded into {path}!");
                 const EXAMPLE_CONTENT: &str = include_str!("../examples/life.toml");
 
-                dbg!(&FILE_PATH.as_path());
                 write_to_file(&FILE_PATH, EXAMPLE_CONTENT)
-            } else {
-                Ok(())
             }
-        }
-    }
-    fn process_request(&self, req: &mut ChatCompletionRequest, args: &Args) -> Result<()> {
-        let Args {
-            execute, example, ..
-        } = args;
+            Args {
+                input_path: Some(ref input_path),
+                execute,
+                ..
+            } => {
+                let contents = read_file(input_path)?;
+                let mut req = toml::from_str(contents.as_str())?;
 
-        // TODO codesmell: match refactor needed
-        if *example {
-            lazy_static! {
-                static ref FILE_PATH: PathBuf = PathBuf::from("new.toml");
-            };
-            const EXAMPLE_CONTENT: &str = include_str!("../examples/life.toml");
+                if execute {
+                    if let Some(command) = get_command(&req)? {
+                        let output = run(&command)?;
+                        req.messages.push(ChatCompletionMessage {
+                            role: MessageRole::user,
+                            content: format!("output of command:\n```\n{output}\n```\n"),
+                            name: None,
+                            function_call: None,
+                        });
+                    }
+                    Ok(())
+                } else {
+                    // TODO move continue_chat to ClientExt trait from dependency
+                    self.continue_chat(&mut req)?;
 
-            return write_to_file(&FILE_PATH, EXAMPLE_CONTENT);
-        }
-
-        if *execute {
-            if let Some(command) = get_command(req)? {
-                let output = run(&command)?;
-                req.messages.push(ChatCompletionMessage {
-                    role: MessageRole::user,
-                    content: format!("output of command:\n```\n{output}\n```\n"),
-                    name: None,
-                    function_call: None,
-                });
+                    write_to_file(input_path, &toml::to_string(&req)?)
+                }
             }
-        } else {
-            // TODO move continue_chat to ClientExt trait from dependency
-            self.continue_chat(req)?;
+            _ => Ok(()),
         }
-
-        Ok(())
     }
 }
 
